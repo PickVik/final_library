a794eb485650cf8a8854267e36976018eb487517
pallavidughrekar <38434226+pallavidughrekar@users.noreply.github.com>
2018-05-07 17:43:36 +0100

    book with style

--- insert_books.php
+++ insert_books.php
@@ -4,21 +4,21 @@ include 'book_class.php';
 
 if (!empty($_POST)) {
     
-   
-    if (isset($_POST['Delete'])){
-    echo 'delete';
-    $Book = new Books();
-    
-    $Book->delete($_POST['Title']);
-    echo 'delete end';
-}
-if (isset($_POST['Insert'])){
-        echo 'insert';
+   if (isset($_POST['Insert'])){
+        //echo 'insert';
         $Book = new Books();
-        echo 'insert end';
+        //echo 'insert end';
         $Book->insert($_POST['ISBN'], $_POST['Title'], $_POST['Type'], $_POST['Genre'], $_POST['Price'], $_POST['Borrow_status']);
                    
     }
+    if (isset($_POST['Delete'])){
+    //echo 'delete';
+    $Book = new Books();
+    
+    $Book->delete($_POST['Title']);
+    //echo 'delete end';
+}
+
 
     if (isset($_POST['Update'])){
     
@@ -30,36 +30,15 @@ if (isset($_POST['Insert'])){
     
     $Book = new Books();
     $results= $Book->search($_POST['ISBN'], $_POST['Title'], $_POST['Type'], $_POST['Genre'], $_POST['Price'], $_POST['Borrow_status']);
-   ?>
+   ?>           
 
 
-        <table>
-            <thead>
-                <th>Book Title</th>
-                <th>Borrow status</th>
-            </thead>
-            <tbody>
+                  
+                  
+
+
+            
    
-<?php
-
-foreach($results as $result) {
-                        
-            echo "<tr>";
-            echo "<td>{$result['Title']}</td>";
-            echo "<td>{$result['Borrow_status']}</td>";
-            echo "</tr>";
-}
-
-?>
-
-                 </tbody>
-        </table>
-<?php
-    }
-    
-}
-    ?>
-
 
 
 
@@ -71,30 +50,81 @@ foreach($results as $result) {
         <title>Library - Insert Books</title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.8/css/solid.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+        <link href="book.css" rel="stylesheet" type="text/css"/>
     </head>
     <body>
         
-        
-        <h1>Welcome to the library!</h1>
-        <h2>Please insert a new book!</h2>
-        
+        <div class="container-fluid">
+             <h1>Welcome to PikVik library!</h1>
+             </div>
+               <div class="container-fluid">
+       
+                  <p>You can update the library here.</p>
+               </div>
+                <div class="col-8">
+                <img src="search.jpg" alt="book search" class="float-right"  /> 
+            </div> 
+ 
     <form action="" method="post" > 
-        
-        
-     ISBN:          <input type="int" name="ISBN" /> <br><br>
-     Title:         <input type="text" name="Title" /> <br><br>
-    Type:           <input type="text" name="Type" /> <br><br>
-    Genre:          <input type="text" name="Genre" /> <br><br>
-    Price:          <input type="text" name="Price" /> <br><br>
-    Borrow_status:  <input type="text" name="Borrow_status" /> <br><br>
-    <div>
+        <div class="container"> 
+             <div> 
+                 <strong>ISBN:   </strong><input type="int" name="ISBN" /> <br><br>
+                 <strong>Title:  </strong> <input type="text" name="Title" /> <br><br>
+                 <strong>Type:   </strong> <input type="text" name="Type" /> <br><br>
+                 <strong>Genre:  </strong> <input type="text" name="Genre" /> <br><br>
+                 <strong>Price:  </strong><input type="text" name="Price" /> <br><br>
+                 <strong>Borrow_status:   </strong><input type="text" name="Borrow_status" /> <br><br>
+            <div>
                     <input type="submit" name="Insert" value="Insert" /> 
                     <input type="submit" name="Delete" value="Delete" />
                     <input type="submit" name="Update" value="Update" />
                     <input type="submit" name="Search" value="Search" />
-    </div>
+                    
+             </div>
+        </div>
     </form>
-        
+</div> 
+        <div class="container" style="align-self: flex-end">
+        <div class="col-3">     
+        <table>
+            <thead>
+            <strong> <th>Book Title</th></strong>
+            <strong> <th>Borrow status</th></strong>
+            </thead>
+            <tbody>
+                
+
+<?php foreach($results as $result) {?>
+            
+              <div class="row result">
+                 <div class="col-12">   
+                  
+                      <?php echo "<tr>";?>
+                        <?php echo "<td>{$result['Title']}</td>";?>
+                         <?php echo "<td>{$result['Borrow_status']}</td>";?>
+                       <?php echo "</tr>";?>
+                 </div>
+              </div>
+              
+             </tbody>
+                 
+        </table>
+</div>
+                  
+<?php }
+    }
+?>                 
+         
+  <?php }?>
+ 
+
     </body>
+      
+
 </html>
     
