07641c7d8946d7da3c9aa65669b868df2e3c3e61
Claire <clairecunliffe@gmail.com>
2018-05-08 22:03:13 +0100

    small formatting tweak on borrower_search, edit to borrow_book for user to borrow book (still not working - need edits to table)

--- borrow_book.php
+++ borrow_book.php
@@ -1,14 +1,27 @@
 <?php
 
 session_start();
-print_r ($_SESSION);
-//need user ID in the session scope as well as the email address
+require_once 'user_class.php';
+//print_r ($_SESSION);
+$validateduser = new User;
+//print_r ($validateduser);
+$results= $validateduser->search();
+
+//foreach($results as $result){
+//        echo $result['ID'];
+//}
+
+$loggedinuser = $results[0];
+
+
 include 'booksearch_class.php';
 
   if (!empty($_GET)) {
 
         $books = new Booksearch();
         //User ID hard coded as 2 for now as not available in session scope
-        $results = $books->updateborrower(2, $_GET['ISBN']);
-//        print_r ($results);
+        $results = $books->updateborrower($loggedinuser['ID'], $_GET['ISBN']);
+
     }
+
+   
