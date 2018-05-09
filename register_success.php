31d627616c9bd02f7d4ab2ca4ffd51766dd841cb
johasago <jo@gowithjo.online>
2018-05-09 12:33:23 +0100

    register success

---
+++ register_success.php
@@ -0,0 +1,23 @@
+<?php
+
+?>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+        <link rel="stylesheet" href="signup.css">
+
+        <title>Registration page</title>
+    </head>
+    <body>
+        <header>
+      <div class="jumbotron jumbotron-fluid">
+        <h1 id="register">Thank you for Registering!</h1>
+     </div>
+        </header>
+        <div class="container" align="center">
+            <img src='Smiley-Face-05-large.png' alt='Smiley face' height='150' width='150'> 
+            <h3>Now please click <a href='login.php'>here</a> to log in</h3>
+        </div>
+
